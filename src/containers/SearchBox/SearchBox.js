c05d4de69681b528e4ca5efec72faf6a0fb99b36
Arfian Adam <arfianadam@yahoo.com>
2017-09-13 01:34:48 +0700

    enhanced searchbox with loading state

--- src/containers/SearchBox/SearchBox.js
+++ src/containers/SearchBox/SearchBox.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { getRepos } from 'redux/modules/repos';
 import styles from './SearchBox.scss';
 
+@connect(state => ({
+  requests: state.requests
+}))
 export default class SearchBox extends Component {
+  static propTypes = {
+    dispatch: PropTypes.func.isRequired,
+    requests: PropTypes.array.isRequired
+  }
+
   constructor(props) {
     super(props);
     this.state = {
@@ -15,15 +27,37 @@ export default class SearchBox extends Component {
     });
   }
 
-  render() {
+  submit = e => {
+    e.preventDefault();
+    const { dispatch } = this.props;
     const { value } = this.state;
+    dispatch(getRepos(value));
+  }
+
+  render() {
+    const { requests } = this.props;
+    const { value } = this.state;
+    const isLoading = requests.filter(req => req.loading).length > 0;
     return (
       <div className={styles.SearchBox}>
-        <input
-          type="text"
-          value={value}
-          onChange={this.handleChange}
-          placeholder="Insert organization slug.." />
+        <div className="form-group">
+          <form onSubmit={this.submit}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Insert organization slug.."
+              value={value}
+              disabled={isLoading}
+              onChange={this.handleChange} />
+            <button
+              className="btn btn-primary"
+              disabled={isLoading}
+              type="submit">Go!</button>
+          </form>
+        </div>
+        {isLoading &&
+          <span><i className="fa fa-spinner fa-pulse" aria-hidden="true" />&nbsp;This might take a while..</span>
+        }
       </div>
     );
   }
