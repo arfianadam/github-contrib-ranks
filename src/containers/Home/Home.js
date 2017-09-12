b6917b8120fcbbda8f00cf4142594ad8ae513a49
Arfian Adam <arfianadam@yahoo.com>
2017-09-12 22:18:37 +0700

    clean Home component

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
-import { connect } from 'react-redux';
 
-@connect(
-  state => ({
-    online: state.online
-  })
-)
+import styles from './styles.scss';
+
 export default class Home extends Component {
   render() {
     return (
-      <div className="container">
-        <Helmet title="Home" />
-        <h1>Hello world!</h1>
+      <div className={styles.Home}>
+        <div className="container">
+          <Helmet title="Home" />
+          <header>
+            <h1>Github Contributors Rank by Organization</h1>
+          </header>
+        </div>
       </div>
     );
   }
